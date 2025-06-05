ba3076ac3f87ff7b3960eea2565d80f2d6a0a0d9
Anton Vovk <143019553+infisble@users.noreply.github.com>
2025-06-06 01:51:01 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,16 +1,27 @@
 # database.py
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
-
-from models import Base, User
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import Column, BigInteger, Integer, String
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
-# sessionmaker for creating async database sessions
-async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+# Session factory for creating asynchronous sessions
+async_session = sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    telegram_id = Column(BigInteger, unique=True, index=True)
+    phone = Column(String, unique=True)
 
 async def init_db():
     async with engine.begin() as conn:
